12bdd6675ade1869652197c206426786c5227cc4
Twan Huiskens <huiskenstwan@gmail.com>
2019-11-19 21:06:38 +0100

    Update to reflect changes in prev commit (see #2 for reference)

--- show_img.py
+++ show_img.py
@@ -2,13 +2,27 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-from process_img import FaceData
+import random
 
-FD = FaceData()
-training_data = FD.gather_folders_and_labels()
-labels = FD.FACES_LABELS
-print(training_data[0])
+### OLD TEST
+# from process_img import FaceData
+# FD = FaceData()
+# training_data = FD.gather_folders_and_labels()
+# labels = FD.FACES_LABELS
+# print(training_data[0])
+# print()
+# print(labels[np.where(training_data[0][1] == 1.)[0][0]])
+# plt.imshow(training_data[0][0])
+# plt.show()
+
+
+file_data = np.load('training_data.npy', allow_pickle=True)
+training_data = file_data[0]
+randon_nr = random.randint(0, (len(training_data)-1))
+labels = file_data[1]
+
+print(training_data[randon_nr])
 print()
-print(labels[np.where(training_data[0][1] == 1.)[0][0]])
-plt.imshow(training_data[0][0])
+print(labels[np.where(training_data[randon_nr][1] == 1.)[0][0]])
+plt.imshow(training_data[randon_nr][0])
 plt.show()
